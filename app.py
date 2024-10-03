415c51ce31f4fc429f2626215e434ba8e4e51a9f
CareyFang <carey020281@gmail.com>
2024-10-04 05:51:05 +0800

    Patched

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, render_template_string
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -60,17 +61,15 @@ def level6():
 
 @app.route('/level1/frame', methods=['GET'])
 def level1_frame():
-    # Disable the reflected XSS filter for demonstration purposes
-    # Note: Flask doesn't have XSS protection by default, so this line is not needed.
-
     query = request.args.get('query')
     
     if not query:
         # Show main search page
         content = page_header + main_page_markup + page_footer
     else:
-        # Vulnerability: directly use the query parameter without sanitization
-        message = f"Sorry, no results were found for <b>{query}</b>."
+        # Escape the query parameter to prevent XSS
+        safe_query = escape(query)
+        message = f"Sorry, no results were found for <b>{safe_query}</b>."
         message += " <a href='?'>Try again</a>."
         content = page_header + message + page_footer
     
@@ -104,28 +103,42 @@ def level4_source():
 def level4_frame():
     timer = request.args.get('timer')
     if not timer:
-        # Show the main timer page (frame.html)
         return render_template('levels/level4/frame.html')
     else:
-        # Show the timer page (timer.html) with the timer value
-        return render_template('timer.html', timer=timer)
+        # Validate timer input
+        try:
+            timer_int = int(timer)
+            # Enforce acceptable range
+            if timer_int < 1 or timer_int > 99999:
+                timer_int = 3  # Default value
+        except ValueError:
+            timer_int = 3  # Default value if parsing fails
+        return render_template('timer.html', timer=timer_int)
+
 
 @app.route('/level5/source')
 def level5_source():
     return render_template('levels/level5/source.html')
 
+# Define allowed paths for redirection
+ALLOWED_NEXT_PATHS = ['confirm', 'welcome']
+
 @app.route('/level5/frame', methods=['GET'])
 def level5_frame():
-    return render_template('levels/level5/frame.html')
+    return render_template('welcome.html')
 
 @app.route('/level5/frame/signup', methods=['GET'])
 def level5_frame_signup():
-    next_param = request.args.get('next')
+    next_param = request.args.get('next', 'confirm')
+    if next_param not in ALLOWED_NEXT_PATHS:
+        next_param = 'confirm'
     return render_template('signup.html', next=next_param)
 
 @app.route('/level5/frame/confirm', methods=['GET'])
 def level5_frame_confirm():
     next_param = request.args.get('next', 'welcome')
+    if next_param not in ALLOWED_NEXT_PATHS:
+        next_param = 'welcome'
     return render_template('confirm.html', next=next_param)
 
 @app.route('/level5/frame/welcome', methods=['GET'])
